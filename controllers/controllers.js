d695af7ca235aca562488cd24ed9aa45c2b0c4c8
khudadad414 <khudadad414@gmail.com>
2021-02-07 16:02:46 +0530

    function in function resolved

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,8 @@
 exports.connectDatabase = async function() {
     const mongodb = require('mongodb');
     const MongoClient = mongodb.MongoClient;
-  //  const url = 'mongodb+srv://temp-user:temp-password@sandbox.vawr3.gcp.mongodb.net/Airport';
-      const url = 'mongodb://localhost';
+    const url = 'mongodb+srv://temp-user:temp-password@sandbox.vawr3.gcp.mongodb.net/Airport';
+  //    const url = 'mongodb://localhost';
     const dbName = "Airport";
 
     if (!global.db) {
